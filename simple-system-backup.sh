ab07254108efd555fcd01c832731f8d76d5edfc9
vlad.kyiashko <vlad.kyiashko@gmail.com>
2022-06-26 23:51:51 +0300

    Initial commit

---
+++ simple-system-backup.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+BACKUP_PATH=/mnt/630789e0-1fd4-4bf9-93c0-ee3dd0bf9a5a/backups
+EXCLUDE_DIRS=("/dev/*" "/proc/*" "/sys/*" "/tmp/*" "/run/*" "/mnt/*" "/media/*" "/lost+found" "/lost+found/*" "/home/*/.cache/*" "/var/lib/dhcpcd/*")
+CURRENT_DIR=backup
+PREV_DIR=prev_backup
+
+check_create_backup_dirs() {
+	dirs=($BACKUP_PATH/$CURRENT_DIR $BACKUP_PATH/$PREV_DIR)
+	for dir in ${dirs[@]}; do
+		if [ ! -d $dir ]; then
+			echo "mkdir $dir"
+			if ! mkdir -p $dir; then
+				echo "Can't create folder at path $dir; PLEASE SET THE CORRECT BACKUP_PATH in the script; exit"
+				exit 0
+			fi
+		fi
+	done
+}
+
+confirm() {
+	while true; do
+		read -p "$1" yn
+		case $yn in
+			[yY]) break;;
+			[nN]) exit 1;;
+		esac
+	done
+}
+
+do_rsync() {
+	dirs=($1 $2)
+	for dir in ${dirs[@]}; do
+		if [ ! -d $dir ]; then
+			echo "No folder at path $dir; exit"
+			exit 0
+		fi
+	done
+
+	EXCLUDE_ARG=(--exclude="$BACKUP_PATH/*")
+	for path in "${EXCLUDE_DIRS[@]}"; do
+		EXCLUDE_ARG+=(--exclude="$path")
+	done
+	
+	rsync -aAXH --info=progress2 --delete "${EXCLUDE_ARG[@]}" $1/* $2
+}
+
+case "$1" in
+	"--create")
+		check_create_backup_dirs
+		confirm "Create backup? (y/n) " 
+		do_rsync / $BACKUP_PATH/$CURRENT_DIR
+		exit 1;;
+	"--create-with-prev")
+		check_create_backup_dirs
+		confirm "Create backup? (y/n) " 
+		if [ "$(ls -A $BACKUP_PATH/$CURRENT_DIR)" ]; then
+			do_rsync $BACKUP_PATH/$CURRENT_DIR $BACKUP_PATH/$PREV_DIR
+		fi
+		do_rsync / $BACKUP_PATH/$CURRENT_DIR
+		exit 1;;
+	"--restore")
+		confirm "Restore backup? (y/n) " 
+		do_rsync $BACKUP_PATH/$CURRENT_DIR /
+		exit 1;;
+	"--restore-prev")
+		confirm "Restore previous backup? (y/n) " 
+		do_rsync $BACKUP_PATH/$PREV_DIR /
+		exit 1;;
+	*)
+		echo "Invalid argument; try '--create' or '--create-with-prev' or '--restore' or '--restore-prev'"
+		exit 0;;
+esac
